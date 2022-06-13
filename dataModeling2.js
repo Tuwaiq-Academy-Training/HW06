dd7d2895d5556b528af17b24afb90616e9cf655b
YazeedAlhanaki <96131525+YazeedAlhanaki@users.noreply.github.com>
2022-06-14 00:04:42 +0300

    Yazeed

---
+++ dataModeling2.js
@@ -0,0 +1,131 @@
+
+const InHouseRepositories = [
+    repos = {
+        repositries: [repositries],
+        repoName : String,
+        PublicOrPrivate: Boolean,
+        TypeOflanguage: String,
+        numberOfForks: Number,
+        updateTime: Date,
+        favouriteStar: Boolean,
+        pullRequests: Number
+    },
+
+
+    PullRequestsFuatures = {
+
+        labels:[labels],
+        //  Labels:
+        bug: String,
+        ducomentaion: String,
+        duplicate: Boolean,
+        enhancement: String,
+        helpWanted: Boolean,
+        invalid: Boolean,
+        question: String,
+        wontfix: Boolean,
+
+        
+        milestones: [milestones],
+        // milestines
+        title: String,
+        dueDate: Date,
+        lastUpdated: Date,
+        complete: Number,
+        open: Number,
+        closed: Number,
+
+        pullRequests: [pullRequests],
+        //  pull request details
+        selected: Boolean,
+        open: String,
+        closed:String,
+        title: String,
+        timeOpened: Date,
+        author: [String],
+        label: [labels],
+        // labels array
+        bug: String,
+        ducomentaion: String,
+        duplicate: Boolean,
+        enhancement: String,
+        helpWanted: Boolean,
+        invalid: Boolean,
+        question: String,
+        wontfix: Boolean,
+
+
+        projects: [projects],
+
+
+        reviews: [reviews],
+        // reviews details
+        noReviews: String,
+        reviewRequired: String,
+        approvedReview: String,
+        changesRequest: String,
+        reviewByYou: String,
+
+        milestones: [milestones],
+        assignee: [users],
+        sort: [sort],
+        // sorting array
+        newest: String,
+        oldest: String,
+        mostCommentes: String,
+        leastCommentes: String
+
+
+
+    },
+
+
+    IssuesFeatures = {
+        
+        labels:[labels],
+        //  Labels:
+        bug: String,
+        ducomentaion: String,
+        duplicate: Boolean,
+        enhancement: String,
+        helpWanted: Boolean,
+        invalid: Boolean,
+        question: String,
+        wontfix: Boolean,
+
+        milestones: [milestones],
+        issues: [issues],
+        selected: Boolean,
+        open : Number,
+        closed: Number,
+        author: [users],
+        open: String,
+        closed:String,
+        title: String,
+        timeOpened: Date,
+
+
+        projects: [projects],
+        
+        label: [labels],
+        // labels
+        bug: String,
+        ducomentaion: String,
+        duplicate: Boolean,
+        enhancement: String,
+        helpWanted: Boolean,
+        invalid: Boolean,
+        question: String,
+        wontfix: Boolean,
+
+        sort: [sort],
+        // sorting array
+        newest: String,
+        oldest: String,
+        mostCommentes: String,
+        leastCommentes: String
+
+
+    
+    }
+]
