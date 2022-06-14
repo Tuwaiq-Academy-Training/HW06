b6b9853c8bd24ca7122d7d977b7144af9fb0a236
alWasayf <49132323+alWasayf@users.noreply.github.com>
2022-06-14 08:49:15 +0300

    homework6Wasayf

---
+++ foodApp.js
@@ -0,0 +1,20 @@
+meal:{
+   'id'=Number,
+   'name'=string,
+   'description'=string,
+   'note'=[string],
+   'calories'=Number
+}
+
+user:{
+    "name"=String,
+    'age'=Number,
+    'gender'=string,
+    "goal"=String,
+    'meals'=[meal],
+    'activeExersice'=[sting],
+    'height'=Number,
+    'weight'=Number,
+    'goalWeight'=Number,
+    'weeklylossWeight'=Number //If this app use to follow weight loss
+}
