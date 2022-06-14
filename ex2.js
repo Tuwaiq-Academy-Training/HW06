4c58f3a5f72671bb5348b1034e27f1cd7c0dc768
Meshal1172 <mr.meeshal@hotmail.com>
2022-06-14 09:21:44 +0300

    Meshal'

---
+++ ex2.js
@@ -0,0 +1,35 @@
+ user={
+    "_id":number,
+    "email":string,
+    "username":string,
+    "password":string,
+}
+repositories={
+    "repo_id":number,
+    "user_id":number,
+    "username":string,
+    "name_repo":string,
+    "link":string,
+
+}
+ pullRequest={
+
+   "user_id":number,
+   "username":string,
+    "link_repo":string,
+   " open": string,
+   " closed":string,
+   " title": string,
+   " author": string,
+   " label": string,
+
+}
+
+issues={
+    "_id_issues":string,
+    "type_issues":string,
+    "Created":string,
+    "assigend":string,
+    "description_issues":string,
+
+} 
