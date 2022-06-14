6ea53b677cdfc8de7c051e39d4fea833e7854718
lamia-x5 <lamia-abdullah@outlook.com>
2022-06-14 08:37:08 +0300

    Done

---
+++ In_House.js
@@ -0,0 +1,32 @@
+
+
+const InHouses=[
+
+    Repositories={
+        "Pull requests":[],
+        "Repositories":[],
+        "Issues":[],
+        "projectName":[],
+        "NumberOfsort":[],
+        "author":[],
+        "watching":[],
+        "stars":[],
+    },
+
+
+Pull_requests={
+"Pull_requests":Number,
+"state":"string",
+"fork":Number,
+"name":"string",
+
+},
+
+Issues={
+"author":"string",
+"id":Number,
+
+},
+
+
+]
