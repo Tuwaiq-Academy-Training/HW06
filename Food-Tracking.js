a4f44d7a2fac17ec2ee7be45d72516f7950c5924
Lamis Abdullah <lamisabdullah@outlook.com>
2022-06-14 05:15:44 +0300

    LLamiss

---
+++ Food-Tracking.js
@@ -0,0 +1,29 @@
+const track=[
+    Meals= {
+        "BREAKFAST":[string],
+        "LAUNCH":[string],
+        "DRINKS":[string],
+        "COFFEE":[string],
+        "SNACK":[string],
+        "DINNER":[string]
+    },
+    contentMeal={
+        "Ingredients":[string],
+        "Carp":[Number],
+        "fats":[string],
+        "Quantity":[Number]
+    },
+    Calories ={
+        "BREAKFAST":[Number],
+        "LAUNCH":[Number],
+        "DINNER":[Number]
+    },
+    Target={
+        "targetWeight":[Number]
+    },
+    Exercise={
+        "swimming":[Number],
+        "cardio":[Number],
+        "climbingStairs":[Number]
+    }
+]
